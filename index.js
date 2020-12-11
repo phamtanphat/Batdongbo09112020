340a74959ad7fa269a3bdc4d2f372d57d86ffb35
phamtanphat <phatdroid94@gmail.com>
2020-12-11 18:21:54 +0700

    callback

--- index.js
+++ index.js
@@ -13,9 +13,17 @@
 // }
 
 const request = require('request');
-const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=Hanoi';
-request(URL, function (error, response, body) {
-    console.log(body);
-});
 
-getTempCity(cityname , cb())
+// back tick : `${}`
+function getTempCity(cityName , cb) {
+    const URL = `http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityName}`;
+    request(URL, {json : true} ,function (error, response, body) {
+        if (error) return cb(error)
+        if (body.cod !== 200) return cb(body.message)
+        return cb(null , body.main.temp)
+    });
+}
+
+getTempCity('Hanoi 2321',(error , temp) => {
+    console.log(error || temp)
+})
